271ca176e45706da33e1e5645df33735d202922f
sandhya konda <kondasandhya64@gmail.com>
2024-04-26 09:41:37 +0530

    innovatedEd solutions

---
+++ tribute page/css/LandingPage/LandingPage.js
@@ -0,0 +1,10 @@
+
+let menuIcon = document.querySelector("#menu-icon");
+let navBar = document.querySelector(".navbar");
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+let home = document.querySelectorAll("#home");
+console.log(menuIcon,navBar,sections,navLinks,home)
+
+    
